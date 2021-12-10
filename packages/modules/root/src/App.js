a2baefc0cd11148eba35f834def0c2b804bbc46e
Francisco Quarato <fquarato@dminc.com>
2021-12-10 12:36:04 -0300

    Add changes

---
+++ packages/modules/root/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import { Header } from "@fquarato/components"
+
+function App() {
+  return (
+    <div className="App">
+      <Header text="from root" />
+    </div>
+  )
+}
+
+export default App
